e3c382c25a050dbd6198abc3871910a9edb13948
yigao <445267987@qq.com>
2022-06-24 22:37:35 +0800

    add robot

--- Install/Bin/Debug/rund_robot.bat
+++ Install/Bin/Debug/rund_robot.bat
@@ -1,8 +1,8 @@
 cd /d %~dp0
 cd Debug
 
-echo Starting AllServer...
-start "AllServer" "NFPluginLoader.exe" "--Server=AllServer" "--ID=1.1.1.1" "--Config=../../Config" "--Plugin=../../TcpPlugin"
+echo Starting Robot...
+start "Robot" "NFPluginLoader.exe" "--Server=RobotServer" "--ID=1.1.1.1" "--Config=../../Config" "--Plugin=../../TcpPlugin" "--Param=33,1,127.0.0.1,7051"
 
 
 
